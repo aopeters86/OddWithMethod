63d142c3371f1c69dab8b9140644b62781c98c21
aopeters86 <114446425+aopeters86@users.noreply.github.com>
2022-12-13 14:08:51 -0600

    Update OddWithMethod.java

--- OddWithMethod.java
+++ OddWithMethod.java
@@ -3,18 +3,24 @@ public class OddWithMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//how to make this print horiz?
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("Enter a city: ");
 		String cityIn = keyboard.nextLine();
-		
+		Odd(cityIn);
 
 	}
-		public String Odd(String x){
-		for(int i = 0; i < x.length() ;i = i + 2) {
-		return x.String(charAt(i));
-	}
+	
+	
+		public static void Odd(String x){
+		for(int i = 0; i < x.length();i = i + 2) {
+		 char y = x.charAt(i);
+		 System.out.print(y);
+		}
+		
+		
+		
 
 }
 }
